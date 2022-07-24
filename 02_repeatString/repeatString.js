a8a6d361052fb832f63343c5ff41052911aacd61
Gulov Azam <gulovazam@gmail.com>
2022-07-24 23:01:05 +0500

    Solved assignment 2 using while loop and for loop.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,26 @@
-const repeatString = function() {
+// const repeatString = function(string, num) {
+//     let result = '';
+//     if (num < 0) {
+//         return "ERROR";
+//     } else {
+//     while (num !== 0) {
+//         result += string;
+//         num--; 
+//     }
+//     return result;
+// }
+// };
 
-};
+const repeatString = function(string, num) {
+    let result = '';
+    if (num < 0) {
+        return "ERROR";
+    }
+    for (; num > 0; num--) {
+        result += string;
+    }
+    return result;
+}
 
 // Do not edit below this line
 module.exports = repeatString;
