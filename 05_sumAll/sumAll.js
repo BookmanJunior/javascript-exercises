ab0797f914d5cbe45fec75704969deaa18d8acef
Gulov Azam <gulovazam@gmail.com>
2022-07-25 13:48:25 +0500

    Passed all tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,10 +1,14 @@
 const sumAll = function(num1, num2) {
     let result = 0;
-    if (num1 > num2) {
-        for (num1; num1 > num2; num1--) {
-            result += num1;
-        }
+    if (num1 < 0 || num2 < 0 || typeof num1 !== "number"
+     || typeof num2 !== "number") {
+        return "ERROR";
     }
+    else if (num1 > num2) {
+        for (num1; num1 >= num2; num1--) {
+            result += num1;
+        } 
+    } 
     for (num1; num1 <= num2; num1++) {
         result += num1;
     }
