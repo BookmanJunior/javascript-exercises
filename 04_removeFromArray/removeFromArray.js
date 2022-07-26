e675fb3c1ec057c65993e3aa5bf168a973858dfa
Gulov Azam <gulovazam@gmail.com>
2022-07-26 09:52:22 +0500

    Passed all tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(...args) {
+    const array = args[0];
+    return array.filter(val => !args.includes(val));
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+let newItems = removeFromArray([1, 2, 3, 4], 3)
+console.log(newItems)
