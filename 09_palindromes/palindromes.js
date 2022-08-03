b81954f6ac7ade88178e72cff6390499983b2246
Gulov Azam <gulovazam@gmail.com>
2022-08-03 19:24:26 +0500

    Passed all tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  const filteredString = string.toLowerCase().replace(/[^a-z]/g, "");
+  const reversedString = filteredString.split("").reverse().join("");
+  return reversedString === filteredString;
 };
 
 // Do not edit below this line
