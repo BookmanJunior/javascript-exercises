a4b566133c2a545a837206e80cd0bb40221f4903
Gulov Azam <gulovazam@gmail.com>
2022-08-03 11:52:48 +0500

    Passed all tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  const sumOfNumbers = array.reduce(
+    (previousNum, currentNum) => previousNum + currentNum,
+    0
+  );
+  return sumOfNumbers;
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  let total = 1;
+  for (const item of array) {
+    total *= item;
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function (num1, num2) {
+  return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  let total = 1;
+  if (num === 0 || num === 1) {
+    return 1;
+  }
+  for (num; num > 1; num--) {
+    total *= num;
+  }
+  return total;
 };
 
 // Do not edit below this line
@@ -29,5 +44,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
