7e8b65639f0ef30eeaf84fee6eb87e9ba23ff4bc
Gulov Azam <gulovazam@gmail.com>
2022-08-05 16:06:43 +0500

    Passed all tests

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function (array) {
+  const currentYear = new Date().getFullYear();
+  array.forEach((person) => {
+    person.yearOfDeath ??= currentYear;
+  });
 
+  let oldestPerson;
+  const calculateAge = array.reduce((obj, item) => {
+    let calcAge = item.yearOfDeath - item.yearOfBirth;
+    if (calcAge > obj) {
+      obj = calcAge;
+      oldestPerson = item;
+    }
+    return obj;
+  }, 0);
+  return oldestPerson;
 };
 
 // Do not edit below this line
