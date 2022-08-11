6f0199705c37ff07824dbe003b8ebc25a6775e1c
Gulov Azam <gulovazam@gmail.com>
2022-08-11 16:48:36 +0500

    Passed all tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function (input) {
+  const intInput = parseInt(input);
+  const fibbArray = [0, 1];
 
+  if (intInput < 0) return "OOPS";
+
+  if (intInput === 0 || intInput === 1) return fibbArray[intInput];
+
+  while (intInput > fibbArray.length) {
+    const preslastNum = fibbArray.at(-2);
+    const lastNum = fibbArray.at(-1);
+    const newSeq = preslastNum + lastNum;
+    fibbArray.push(newSeq);
+  }
+
+  let result = fibbArray[intInput - 1] + fibbArray[intInput - 2];
+  return result;
 };
 
 // Do not edit below this line
